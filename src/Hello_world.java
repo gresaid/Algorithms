8d524d47f09e01c51288e69da7389bac3a92eacf
gres.aid <gresvladimir02@gmail.com>
2022-09-26 16:36:50 +0600

    string_to_digit //TODO: replace each letter with a number java

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -12,5 +12,6 @@ public class Hello_world{
         out.println("Binary Search (pos) = " + binary_search.binarySearch(myList, 9) + " count step = " + bin_log.binlog(myList.length));
         time = nanoTime() - time;
         out.println( "Time = " + time / 1000000L / 1000.0 + "s ");
+        out.println(string_to_digit.letterToDigit("J a v a"));
     }
 }
---
+++ src/string_to_digit.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class string_to_digit {
+    static ArrayList<String> letter = new ArrayList<>();
+    public static void createAlphabet() {
+        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+            letter.add(String.valueOf(alphabet));
+        }
+
+    }
+    public static String letterToDigit (String input){
+        createAlphabet();
+       String[] splitString = input.toLowerCase().split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String s: splitString){
+            if(letter.contains(s))
+                result.append(Integer.toString(letter.indexOf(s)+1)).append(" ");
+        }
+        return result.toString();
+
+    }
+    }
